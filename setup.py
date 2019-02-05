028c708db8201ca96ad676930a7ba259a9db8ad4
hchareyre <hugo.chareyre@artelys.com>
2019-02-05 11:46:13 +0100

    Update required packages in setup

--- setup.py
+++ setup.py
@@ -10,6 +10,14 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_requires = ["pyproj>=1.9.6",
+                    "geopy>=1.12.0",
+                    "geopandas>=0.4.0",
+                    "networkx>=2.2",
+                    "numpy>=1.15.4",
+                    "shapely>=1.2.18",
+                    "scipy>=1.0.1"]
+
 setuptools.setup(
     name="geonetworkx",
     version="0.1",
@@ -19,6 +27,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
