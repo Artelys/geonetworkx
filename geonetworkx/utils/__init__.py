9c16a1b19f7adf635569659fe2d8bd9345932b4f
hchareyre <hugo.chareyre@artelys.com>
2019-05-07 10:26:29 +0200

    adding __init__ file for utils sub module

---
+++ geonetworkx/utils/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    File name: __init__
+    Author: Artelys
+    Creation date: 06/05/2019
+    Python Version: 3.6
+"""
+
+from geonetworkx.utils.geograph_utils import *
+from geonetworkx.utils.voronoi_utils import *
